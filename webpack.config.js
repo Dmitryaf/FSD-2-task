7592bb5b9a4f08b329c304e44f10f65f641294d6
Dmitry <38415594+Dmitryaf@users.noreply.github.com>
2021-02-22 18:30:20 +0500

    Divided the config into separate files

--- webpack.config.js
+++ webpack.config.js
@@ -1,88 +1,9 @@
-const path = require('path');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CopyPlugin = require('copy-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const HtmlWebpackPugPlugin = require('html-webpack-pug-plugin');
-const configuration = require('webpack');
-console.log(configuration);
+const configuration = require('webpack-config');
 
-const isProd = process.env.NODE_ENV === 'production';
-const isDev = !isProd;
+configuration.environment.setAll({
+  env: () => process.env.NODE_ENV,
+});
 
-const filename = (ext) => (isDev ? `bundle.${ext}` : `bundle.[hash].${ext}`);
-
-const jsLoader = () => {
-  const loaders = [];
-  if (isDev) {
-    loaders.push('eslint-loader');
-  }
-
-  return loaders;
-};
-
-module.exports = {
-  context: path.resolve(__dirname, 'src'),
-  mode: 'development',
-  entry: ['/src/index.js'],
-  output: {
-    filename: filename('js'),
-    path: path.resolve(__dirname, 'dist'),
-  },
-  resolve: {
-    extensions: ['.js'],
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
-    },
-  },
-  devtool: isDev ? 'source-map' : false,
-  devServer: {
-    port: 3000,
-    hot: isDev,
-  },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      fileName: 'src/index.pug',
-      minify: {
-        removeComments: isProd,
-        collapseWhitespace: isProd,
-      },
-    }),
-    new HtmlWebpackPugPlugin(),
-    new CopyPlugin({
-      patterns: [
-        {
-          from: path.resolve(__dirname, 'src/favicon.ico'),
-          to: path.resolve(__dirname, 'dist'),
-        },
-      ],
-    }),
-    new MiniCssExtractPlugin({
-      filename: filename('css'),
-    }),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.s[ac]ss$/i,
-        use: [
-          {
-            loader: MiniCssExtractPlugin.loader,
-            options: {
-              hmr: isDev,
-              reloadAll: true,
-            },
-          },
-          'css-loader',
-          'sass-loader',
-        ],
-      },
-      {
-        test: /\.m?js$/,
-        exclude: /node_modules/,
-        use: jsLoader(),
-      },
-    ],
-  },
-};
+module.exports = new configuration.default().extend(
+  `conf/webpack.[env].config.js`
+);
