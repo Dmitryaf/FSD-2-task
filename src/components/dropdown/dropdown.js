41d66f8525a574fea4d101891a91b2da78937c73
Dmitry <38415594+Dmitryaf@users.noreply.github.com>
2021-03-17 19:43:19 +0500

    Create dropdown component

---
+++ src/components/dropdown/dropdown.js
@@ -0,0 +1,11 @@
+import './dropdown.scss';
+
+class DropDown {
+  constructor($container, options) {
+    this.$container = $container;
+    this.options = options;
+    const input = this.$container.querySelector('input');
+  }
+
+  init() {}
+}
