d3d25371abd27b6220e4f54c6358822e3bacb751
Dmitry <38415594+Dmitryaf@users.noreply.github.com>
2021-03-22 18:36:48 +0500

    Added options

--- src/components/dropdown/index.js
+++ src/components/dropdown/index.js
@@ -5,5 +5,7 @@ const dropdownContainers = $('.js-dropdown');
 
 dropdownContainers.each((_, element) => {
   let $element = $(element);
-  new Dropdown($element, $element.attr('data-type'));
+  let type = $element.attr('data-type');
+  let isVisible = $element.attr('data-visible');
+  new Dropdown($element, { type, isVisible });
 });
