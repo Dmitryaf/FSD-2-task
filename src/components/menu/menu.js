fb8d706d844a51e0db080c2105987b519675953c
Dmitry <38415594+Dmitryaf@users.noreply.github.com>
2021-04-01 20:11:16 +0500

    Added Menu class

--- src/components/menu/menu.js
+++ src/components/menu/menu.js
@@ -1,1 +1,16 @@
-import './menu.scss';
+class Menu {
+  constructor(element) {
+    this.$element = $(element);
+    this.$humburger = this.$element.find('.js-menu__humburger');
+    this.$menu = this.$element.find('.js-menu__list');
+    this.$menuItemDropdown = this.$element.find('.js-menu__item');
+    this.$humburger.on('click', this.openMenu.bind(this));
+  }
+
+  openMenu() {
+    this.$menu.toggleClass('menu__list_open');
+    this.$humburger.toggleClass('menu__humburger_open');
+  }
+}
+
+export default Menu;
